0659e6542112ea379ad7d92b7244bd0fa7d1557c
Levi O Vasconcelos <leviovasconcelos@gmail.com>
2016-09-06 09:44:16 -0300

    procfile reworked

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from movieslib import MoviesLib_app
 
 if __name__ == '__main__':
+   MoviesLib_app.run()
 #   print MoviesLib_app.config['MONGOALCHEMY_CONNECTION_STRING']
-   MoviesLib_app.run(host='0.0.0.0',port=8080)
