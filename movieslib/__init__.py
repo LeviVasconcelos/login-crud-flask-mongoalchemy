b04f43b737bbc8608e868a566064a88d4a6e3829
Levi O Vasconcelos <leviovasconcelos@gmail.com>
2016-09-07 04:12:22 -0300

    removing unecessary commetnary and adding secret_key as environment var aswell

--- movieslib/__init__.py
+++ movieslib/__init__.py
@@ -11,11 +11,10 @@ MoviesLib_app = flask.Flask(__name__)
 MoviesLib_app.config['MONGOALCHEMY_DATABASE'] = 'levimovies'
 MoviesLib_app.config['MONGOALCHEMY_SERVER'] = 'ds019976.mlab.com'
 MoviesLib_app.config['MONGOALCHEMY_PORT'] = '19976'
-MoviesLib_app.config['MONGOALCHEMY_USER'] = os.environ['MONGOLAB_USER'] #movieslibapp
-MoviesLib_app.config['MONGOALCHEMY_PASSWORD'] = os.environ['MONGOLAB_PASSWD'] #movieslibapp_passwd
+MoviesLib_app.config['MONGOALCHEMY_USER'] = os.environ['MONGOLAB_USER'] 
+MoviesLib_app.config['MONGOALCHEMY_PASSWORD'] = os.environ['MONGOLAB_PASSWD'] 
 MoviesLib_app.config['MONGOALCHEMY_SERVER_AUTH'] = False
-MoviesLib_app.config['SECRET_KEY']= 'asdaqwevvxvxvxv'
-#MoviesLib_app.config['MONGOALCHEMY_CONNECTION_STRING'] = 'mongodb://movieslibapp:movieslibapp_passwd@ds01316.mlab.com:1316/levimovies'
+MoviesLib_app.config['SECRET_KEY']= os.environ['FORM_SKEY'] 
 #MoviesLib_app.config['DEBUG'] = True
 
 db = MongoAlchemy(MoviesLib_app)
